779f2ca90cbdfb2881b3455ab8f3cb1f38112b09
Lukas Riho <75627840+lriho@users.noreply.github.com>
2022-08-22 17:10:25 +0300

    remove unnecessary quotes

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ const httpGetAsync = async (url, opts) => new Promise((done, reject) => {
 const run = async (opts, root, postcss) => {
   const httpOpts = {
     headers: {
-      'Connection': 'keep-alive',
+      Connection: 'keep-alive',
       'User-Agent': opts.userAgent,
     },
   };
